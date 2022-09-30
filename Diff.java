ac23dac2f880547dedc9da193427f0ce917e97ec
preeti <preeti003.upreti@gmail.com>
2022-09-30 11:37:03 +0200

    added implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,8 @@ public class SumO
 {  
       public static void main(String args[])   
           {  
-            
+            int n1 = 225, n2 = 115, dif;  
+            dif = n1 - n2;  
+            System.out.println("The sum of numbers is: "+dif); 
            }  
 }  
