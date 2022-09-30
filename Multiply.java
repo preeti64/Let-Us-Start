b557e5494f7d9af470210ed73e7f27b857dc6121
preeti <preeti003.upreti@gmail.com>
2022-09-30 11:16:53 +0200

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class SumO  
+{  
+      public static void main(String args[])   
+          {  
+            int n1 = 225, n2 = 115, mul;  
+            mul = n1 * n2;  
+            System.out.println("The sum of numbers is: "+mul);  
+            System.out.println("Hello"); 
+           }  
+} 
